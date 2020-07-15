6d137b014249075abddd42dc17e08db3460d2539
martarzem <marta.rzempowska@gmail.com>
2020-07-15 23:56:26 +0200

    Dto classes created

---
+++ src/main/java/com/crud/medicalclinic/domain/AppointmentDto.java
@@ -0,0 +1,19 @@
+package com.crud.medicalclinic.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppointmentDto {
+    private Office office;
+    private Patient patient;
+    private Doctor doctor;
+    private LocalDate date;
+    private String purpose;
+    private String status;
+}
